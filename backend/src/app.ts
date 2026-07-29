3ba6725144c43b8fbc3639b8651db28228e99c8a
Qirong <qirongwu18@gmail.com>
2026-07-29 18:49:02 -0400

    feature: Added setup with url routes and entry point for backend

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import cors from "cors"
+import urlRoutes from "./routes/url.routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/urls", urlRoutes);
+
+export default app;
