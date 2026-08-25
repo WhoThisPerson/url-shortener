bdf952f4ff2a2227bc216b5a77a960e7f69e3dd1
Qirong <qirongwu18@gmail.com>
2026-08-25 19:30:50 -0400

    Now adding common/

---
+++ common/constants/http-status.ts
@@ -0,0 +1,17 @@
+/**
+ * Mappings between HTTP status codes and their meanings
+ */
+export const HTTP_STATUS = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+
+    FOUND: 302,
+
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+
+    INTERNAL_SERVER_ERROR: 500
+} as const;
