9c4869265f89e2fb8bb67081b58a84429ff19834
Qirong <qirongwu18@gmail.com>
2026-08-25 19:27:38 -0400

    feat: Added structural components to frontend. Added common/ for shared elements between frontend and backend

--- frontend/src/components/UrlForm/UrlForm.tsx
+++ frontend/src/components/UrlForm/UrlForm.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 
 function UrlForm() {
+
+    const [url, setUrl] = useState("");
+
     return (
-        <div>
-            UrlForm
-        </div>
+        <section>
+            <h2>Enter a URL to shorten</h2>
+
+            <form>
+                <input 
+                    type="url" 
+                    placeholder="Paste here..." 
+                    required 
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                />
+
+                <button type="submit">Shorten</button>
+            </form>
+        </section>
     )
 }
 
