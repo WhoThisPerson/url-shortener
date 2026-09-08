f213b49e52a0c704f4bf3aa4b2e3852446866eb0
Qirong <qirongwu18@gmail.com>
2026-09-08 19:13:34 -0400

    feat: Added copying URL functionality and redirecting to website via clicking shortcode

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,14 @@
 import express from "express"
 import cors from "cors"
 import urlRoutes from "./routes/url.routes.js";
+import redirectRoutes from "./routes/redirect.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/urls", urlRoutes);
+app.use("/", redirectRoutes);
 
 app.get("/", (req, res) => {
     res.send("API is Running")
